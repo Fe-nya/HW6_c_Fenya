4d1c10ef08ddceb2878a9243239d509bece135d6
Fenya <verymagic12@gmail.com>
2023-06-02 10:39:48 +0300

    push

--- 1task_c.c
+++ 1task_c.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int month, year, days;
-    scanf("%d %d", &month, &year);
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
 
-    // Если месяц находится в первой группе, то количество дней равно 31
-    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-        days = 31;
-    } 
-    // Если месяц находится во второй группе, то количество дней равно 30
-    else if (month == 4 || month == 6 || month == 9 || month == 11) {
-        days = 30;
-    } 
-    // Если месяц февраль, проверяем год на високосность
-    else if (month == 2) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            days = 29; // Високосный год, 29 дней
-        } else {
-            days = 28; // Обычный год, 28 дней
-        }
-    } 
+    int x = (a + c - 1) / c * c;
+    while (x <= b && x % d != c) {
+        x += c;
+    }
+    printf("%d ", x);
 
-    printf("%d\n", days); // Вывод количества дней
+    while ((x += c) <= b) {
+        printf("%d ", x * (x % d == c));
+    }
 
     return 0;
 }
